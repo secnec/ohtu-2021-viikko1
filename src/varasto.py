4d9e8ab3b45dd63d5352f778ef3ae5d3f2b3ad62
Antti <secnec@pm.me>
2021-11-14 19:33:37 +0200

    fixed bug

--- src/varasto.py
+++ src/varasto.py
@@ -1,6 +1,6 @@
 class Varasto:
     def __init__(self, tilavuus, alku_saldo = 0):
-        self.tilavuus = tilavuus if tilavuus > 0.0 else 0,0
+        self.tilavuus = tilavuus if tilavuus > 0.0 else 0.0
 
         if alku_saldo < 0.0:
             # virheellinen, nollataan
